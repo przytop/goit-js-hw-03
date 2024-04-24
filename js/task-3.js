c7d824af2ed683cee93958cda40aa0eb31a7fab9
Paweł <102540215+przytop@users.noreply.github.com>
2024-04-25 01:03:37 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function checkForSpam(message) {
+  const message_low = message.toLowerCase();
+  if (message_low.includes("spam",) === true || message_low.includes("sale",) === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
